299acb0a1897e45752441950b54de4dc0010aacc
eduardxdc <eduardxdc@proton.me>
2025-02-27 16:13:39 -0300

    feat: creating the initial project menu structure

--- Program.cs
+++ Program.cs
@@ -6,7 +6,42 @@ namespace TextEditor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to the text editor! Select below which option you want to perform:");
+            Console.WriteLine();
+
+            Console.WriteLine("1 => Open file");
+            Console.WriteLine("2 => Create new file");
+            Console.WriteLine("0 => Exit");
+
+            Console.WriteLine();
+            Console.Write("Enter your choice: ");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: OpenFile(); break;
+                case 2: EditFile(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void OpenFile()
+        {
+
+        }
+
+        static void EditFile()
+        {
+            {
+
+            }
         }
     }
 }
